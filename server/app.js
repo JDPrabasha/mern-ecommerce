e1f5147f73fb3563f3ff7b5879040d4a08aa3000
Dulaj <shad3s.of.m3@gmail.com>
2022-08-30 22:58:26 +0530

    add basic jwt auth

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ var mongoose = require("mongoose");
 
 const bodyParser = require("body-parser");
 
+const jwt = require("jsonwebtoken");
+
 const cors = require("cors");
 //Set up default mongoose connection
 var mongoDB = "mongodb://127.0.0.1/ecommerce";
@@ -18,6 +20,21 @@ db.once("open", function () {
   console.log("Connected to mongodb");
 });
 
+const users = [
+  {
+    id: "1",
+    username: "john",
+    password: "John0908",
+    isAdmin: true,
+  },
+  {
+    id: "2",
+    username: "jane",
+    password: "Jane0908",
+    isAdmin: false,
+  },
+];
+
 const app = express();
 
 app.use(cors());
@@ -26,4 +43,36 @@ app.use(bodyParser.json());
 const productsRoute = require("./routes/products");
 app.use("/products", productsRoute);
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(
+    (user) => user.username === username && user.password === password
+  );
+  if (!user) return res.status(400).send("Invalid username or password");
+  const token = jwt.sign(
+    { userId: user.id, isAdmin: user.isAdmin },
+    "mysecretkey",
+    { expiresIn: "1h" }
+  );
+  res.send({ token, user });
+});
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.status(401).json("You are not authenticated");
+  jwt.verify(token, "mysecretkey", (err, user) => {
+    if (err) return res.status(403).json("Token is invalid");
+    req.user = user;
+    next();
+  });
+};
+
+app.get("/check-role/:userId", verifyToken, (req, res) => {
+  if (req.user.id === req.params.userId || req.user.isAdmin) {
+    res.status(200).json("You have access");
+  } else {
+    res.status(200).json("You do not have access");
+  }
+}),
+  app.listen(3000, () => console.log("Example app listening on port 3000!"));
