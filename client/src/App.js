d2ef34c78ebb2480ac5bdd70788e9f9f9e7754b9
Dulaj <shad3s.of.m3@gmail.com>
2022-08-31 07:19:16 +0530

    add react router and frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import CartCard from "./components/CartCard";
 
 import React, { useState, useEffect } from "react";
 import productsService from "./services/products";
+import { Link } from "react-router-dom";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -14,6 +15,14 @@ function App() {
 
   return (
     <div className="App text-3xl ml-5 mt-5">
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
+      </nav>
       <p className="font-bold text-5xl mb-8">Ecommerce App</p>
       <div className="flex flex-wrap gap-4">
         {products.map((product) => (
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
+import Login from "./routes/login";
+import Register from "./routes/register";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="login" element={<Login />} />
+      <Route path="expenses" element={<Register />} />
+    </Routes>
+  </BrowserRouter>
+);
