b47a53b72b14ee1305db67093f6fd149659d52f5
Dulaj <shad3s.of.m3@gmail.com>
2022-09-07 12:32:05 +0530

    change product page

--- client/src/routes/buyer/product.js
+++ client/src/routes/buyer/product.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { addItem } from "../../slices/cartSlice";
 import productsService from "../../services/products";
+import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
+import { NumberInput } from "@mantine/core";
 
 function Product() {
   const [product, setProduct] = useState({});
@@ -23,14 +25,28 @@ function Product() {
   }, []);
 
   return (
-    <div>
-      <p>{product.name}</p>
-      <p
-        className="bg-blue-100 w-fit px-4 py-1 rounded-lg"
-        onClick={() => dispatch(addItem(product))}
-      >
-        Add to Cart
-      </p>
+    <div className="flex items-center mx-20">
+      <div className="w-1/2">
+        <Image src={product.image} />
+      </div>
+      <div className="w-1/2">
+        <p className="text-3xl font-bold mb-4">{product.name}</p>
+        <p className="mb-4">{product.description}</p>
+        <NumberInput
+          defaultValue={18}
+          min={1}
+          max={100}
+          placeholder="Enter quantity"
+          label="Quantity"
+          withAsterisk
+        />
+        <p
+          className="border-2 w-fit px-4 py-1 rounded-lg mt-8 border-gray-900"
+          onClick={() => dispatch(addItem(product))}
+        >
+          Add to Cart
+        </p>
+      </div>
     </div>
   );
 }
