e8e7b09617b6bbc2bc24785a3caa5252a646310f
Dulaj <shad3s.of.m3@gmail.com>
2022-09-09 18:04:02 +0530

    clean server files

--- server/auth_service/app.js
+++ server/auth_service/app.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const db = require("./db");
+const db = require("../db");
 
 const app = express();
 
