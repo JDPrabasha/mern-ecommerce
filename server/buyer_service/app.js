58cf1cb0755786dfa0c16b32136d4ef477907e35
Nisan Abeywickrama <nisanabeywickrama@gmail.com>
2022-09-10 16:55:20 +0530

    add buyer api

--- server/buyer_service/app.js
+++ server/buyer_service/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const Product = require("../models/Products");
+const db = require("../db");
 const app = express();
 
 app.use(cors());
@@ -9,6 +10,17 @@ app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.status(200).send("Buyer Service");
+});
+
+app.get("/product", (req, res) => {
+  try {
+    Product.find({}, (err, products) => {
+      if (err) res.status(500).end(err);
+      else res.send(products);
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 app.listen(3002, () => console.log("Connected to Buyer Service on port 3002!"));
