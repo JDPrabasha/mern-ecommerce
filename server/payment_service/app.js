2801d9f1a97f9fa5b430e1cce9b7346cada3fcd6
Nisan Abeywickrama <nisanabeywickrama@gmail.com>
2022-09-17 09:24:35 +0530

    add initial payment service

---
+++ server/payment_service/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("../db");
+const Payment = require("../models/Payments");
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/payment", async (req, res) => {
+  res.json({ date: Date.now(), success: true });
+});
